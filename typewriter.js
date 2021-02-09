e3fe20fec8eed7b9f4692c86d66e665a5e00f658
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-09 14:16:11 -0500

    Log string like a typewriter

--- typewriter.js
+++ typewriter.js
@@ -3,7 +3,7 @@ let count = 0;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char); // print the char here
-  }, count); // <= 1s delay to make it noticeable. Can dial it down later.
+  }, count); // <= 50ms delay
   count += 50;
 }
 setTimeout(() => {
